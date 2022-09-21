c87ddf6000efaddbb9f6caf68e3b23983a4a2886
sanjarbek <sanjarsaidov173@gmail.com>
2022-09-21 11:57:01 +0500

    done creating models

--- home_api/models.py
+++ home_api/models.py
@@ -3,8 +3,19 @@ from django.db import models
 class User(models.Model):
     full_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
-    phone = models.PhoneNumberField()
+    phone = models.DecimalField(max_digits=9, decimal_places=0)
+    
+    def __str__(self):
+        return self.username
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     data = models.JSONField()
+    
+    def __str__(self) -> str:
+        return str(self.data)
+
+class Check(models.Model):
+    date = models.DateField(auto_now_add=True)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    isHere = models.BooleanField(default=False)
