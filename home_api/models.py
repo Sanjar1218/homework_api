74ecd835d938fea608a91e64781319ee6dbb4dfc
sanjarbek <sanjarsaidov173@gmail.com>
2022-09-21 10:40:58 +0500

    models created

--- home_api/models.py
+++ home_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    phone = models.PhoneNumberField()
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    data = models.JSONField()
